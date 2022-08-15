925c0222dc3f9cab267be7f7e6831da9e637ddf2
IT19994338 <68724754+Mihisuru@users.noreply.github.com>
2022-08-15 11:55:28 +0530

    Update network.js

--- app/containers/Header/network.js
+++ app/containers/Header/network.js
@@ -1,9 +1,9 @@
 
 const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
-  'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
-  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
-  'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
+  'Local RPC': { rpc: 'HTTP://127.0.0.1:7545', tx_explorer: null },
+  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/v3/1833f975e6a14588bbbce0a91d46078a', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
+  'Main Net': { rpc: 'https://mainnet.infura.io/v3/1833f975e6a14588bbbce0a91d46078a', tx_explorer: 'https://etherscan.io/tx/' },
 };
 
 module.exports = Network;
